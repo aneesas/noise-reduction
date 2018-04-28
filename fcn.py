3f82e89687371caa8b13f2af7ca1a942a9b68a44
Aneesa <aneesasona@gmail.com>
2018-04-28 13:25:55 -0400

    Completed make_data

--- fcn.py
+++ fcn.py
@@ -10,34 +10,151 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
+import glob
+import h5py
+import os
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 INPUT_LENGTH = 512  # number of samples in each input waveform
 NUM_FILTERS = 15
 FILTER_SIZE = 11
+LEARNING_RATE = 0.001
+BATCH_SIZE = 100
+NUM_EPOCHS = 1
+
+checkpoints_path = "/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/fcn_model"
+data_path = "/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/data/"
 
 # 1 convolutional layer, with padding (i.e., zero-pad so that output size = input size)
 # 15 filters, each with a filter size of 11
 # ReLU activation functions, Adam training w/batch normalization
 # Objective function: minimize MSE between clean and noisy waveforms
 
-def fcn_model(noisy_in, clean_exp, mode):
+def fcn_model(features, labels, mode):
     """Model function for FCN"""
     # Input layer
-    input_layer = tf.reshape(noisy_in["x"], [-1, INPUT_LENGTH, 1])
+    input_layer = tf.reshape(features["x"], [-1, INPUT_LENGTH, 1])
 
     conv1 = tf.layers.conv1d(
-        inputs = input_layer,
-        filters = NUM_FILTERS,
-        kernel_size = FILTER_SIZE,
-        padding = 'same',
-        activation = tf.nn.relu)
+        inputs=input_layer,
+        filters=NUM_FILTERS,
+        kernel_size=FILTER_SIZE,
+        padding="same",
+        activation=tf.nn.relu)
 
     loss = tf.losses.mean_squared_error(
-        clean_exp,
+        labels,
         conv1,
-        reduction = tf.losses.Reduction.MEAN)
+        reduction=tf.losses.Reduction.MEAN)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=LEARNING_RATE)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode,
+            loss=loss,
+            train_op=train_op)
+
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels,
+            predictions=conv1)}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+    print("Loading train and eval data")
+    # Load training and eval data
+    # Data should have dimensions (number of signals, signal length in samples)
+
+    if os.path.exists(data_path+"training_data_full.npz"):
+        print("Loading train data from npz file")
+        npzfile = np.load(data_path+"training_data_full.npz")
+        train_data_noisy = npzfile["noisy"].astype('float32')
+        train_data_clean = npzfile["clean"].astype('float32')
+    else:
+        train_data_noisy = np.empty((0, INPUT_LENGTH), dtype='float32')
+        train_data_clean = np.empty((0, INPUT_LENGTH), dtype='float32')
+
+        train_data_files = sorted(glob.glob(data_path+"training_data_*.hdf5"))
+        for f in train_data_files:
+            print("Loading from file " + os.path.basename(f))
+            with h5py.File(f, 'r') as fp:
+                clean = fp["clean"].value  # loads as ndarray
+                num_segments = len(clean)//INPUT_LENGTH
+                clean = clean[:num_segments*INPUT_LENGTH]
+                clean_split = clean.reshape((num_segments, INPUT_LENGTH))
+
+                for i in range(1,91):
+                    noisy = fp["n{:03d}".format(i)].value
+                    noisy = noisy[:num_segments*INPUT_LENGTH]
+                    noisy_split = noisy.reshape((num_segments, INPUT_LENGTH))
+                    train_data_noisy = np.vstack((train_data_noisy, noisy_split))
+                    train_data_clean = np.vstack((train_data_clean, clean_split))
+
+        # Save as npz file for easier future use
+        np.savez(data_path+"training_data_full.npz",
+                 noisy=train_data_noisy,
+                 clean=train_data_clean)
+        print("Saved train data as npz")
+
+    if os.path.exists(data_path+"testing_data_full.npz"):
+        print("Loading eval data from npz file")
+        npzfile = np.load(data_path+"testing_data_full.npz")
+        eval_data_noisy = npzfile["noisy"].astype('float32')
+        eval_data_clean = npzfile["clean"].astype('float32')
+    else:
+        eval_data_noisy = np.empty((0, INPUT_LENGTH), dtype='float32')
+        eval_data_clean = np.empty((0, INPUT_LENGTH), dtype='float32')
+
+        eval_data_files = sorted(glob.glob(data_path+"testing_data_*.hdf5"))
+        for f in eval_data_files:
+            print("Loading from file " + os.path.basename(f))
+            with h5py.File(f, 'r') as fp:
+                clean = fp["clean"].value
+                num_segments = len(clean)//INPUT_LENGTH
+                clean = clean[:num_segments*INPUT_LENGTH]
+                clean_split = clean.reshape((num_segments, INPUT_LENGTH))
+
+                for i in range(91, 101):
+                    noisy = fp["n{:03d}".format(i)].value
+                    noisy = noisy[:num_segments*INPUT_LENGTH]
+                    noisy_split = noisy.reshape((num_segments, INPUT_LENGTH))
+                    eval_data_noisy = np.vstack((eval_data_noisy, noisy_split))
+                    eval_data_clean = np.vstack((eval_data_clean, clean_split))
+                noisy = fp["WGN"].value
+                noisy = noisy[:num_segments*INPUT_LENGTH]
+                noisy_split = noisy.reshape((num_segments, INPUT_LENGTH))
+                eval_data_noisy = np.vstack((eval_data_noisy, noisy_split))
+                eval_data_clean = np.vstack((eval_data_clean, clean_split))
+
+        # Save as npz file for easier future use
+        np.savez(data_path+"testing_data_full.npz",
+                 noisy=eval_data_noisy,
+                 clean=eval_data_clean)
+        print("Saved eval data as npz")
+
+    # Create the estimator
+    fcn_DAE = tf.estimator.Estimator(model_fn=fcn_model, model_dir=checkpoints_path)
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data_noisy},
+        y=train_data_clean,
+        batch_size=BATCH_SIZE,
+        num_epochs=None,
+        shuffle=True)
+
+    fcn_DAE.train(input_fn=train_input_fn, steps=20000)
+
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data_noisy},
+        y=eval_data_clean,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = fcn_DAE.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
 
 if __name__ == "__main__":
     tf.app.run()
--- make_data.py
+++ make_data.py
@@ -4,45 +4,122 @@
 
 import os
 import wave
-import nnresample
+#import nnresample
 import struct
 import numpy as np
+import glob
+import h5py
 
-SAMPLE_RATE = 8000 # Hz
+#SAMPLE_RATE = 8000 # Hz
 
-path = '/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/'
-if not os.path.isdir(path+'data'):
-	os.mkdir(path+'data')
+path = '/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/data/'
 
-# Load noise types--will need these repeatedly
-# Resample them to 8 kHz
+# Load noise types
 noise_path = '/Users/aneesasonawalla/Documents/GT/ECE6255/Project/OSU_100noise/'
-training_noise_filenames = ['n001.wav', 'n045.wav', 'n021.wav']
-testing_noise_filenames = ['n089.wav'] # and WGN for stationary noise
+noise_files = sorted(glob.glob(noise_path+'*'))
+training_noise_files = noise_files[:90]
+testing_noise_files = noise_files[90:] # and WGN for stationary noise
 
 train_noise = []
-for filename in training_noise_filenames:
-	fp = wave.open(noise_path+filename, mode='rb')
-	raw_data = fp.readframes(fp.getnframes())
-	signal = struct.unpack('<'+str(fp.getnframes())+'h', raw_data)
-	train_noise.append(nnresample.resample(signal, SAMPLE_RATE,
-										   fp.getframerate()))
-	fp.close()
+for filepath in training_noise_files:
+	with wave.open(filepath, mode='rb') as fp:
+		raw_data = fp.readframes(fp.getnframes())
+		signal = struct.unpack('<'+str(fp.getnframes())+'h', raw_data)
+		#train_noise.append(nnresample.resample(signal, SAMPLE_RATE,
+		#									   fp.getframerate()))
+		train_noise.append(np.array(signal, dtype='int32'))
 
 test_noise = []
-for filename in testing_noise_filenames:
-	fp = wave.open(noise_path+filename, mode='rb')
-	raw_data = fp.readframes(fp.getnframes())
-	signal = struct.unpack('<'+str(fp.getnframes())+'h', raw_data)
-	test_noise.append(nnresample.resample(signal, SAMPLE_RATE,
-										  fp.getframerate()))
-	fp.close()
+for filepath in testing_noise_files:
+	with wave.open(filepath, mode='rb') as fp:
+		raw_data = fp.readframes(fp.getnframes())
+		signal = struct.unpack('<'+str(fp.getnframes())+'h', raw_data)
+		#test_noise.append(nnresample.resample(signal, SAMPLE_RATE,
+		#									  fp.getframerate()))
+		test_noise.append(np.array(signal, dtype='int32'))
 
 
-# Iterate through training data to get names of all files? Randomly pick x of them from list?
+# Training data--all utterances from first female speaker in DR2
+training_data_files = sorted(glob.glob(path+'originals/train/f2*'))
 
-# Then copy each to data/train/clean with the original name
-# Load original, add noise, and save in data/train/noisy
+count = 0
+for filepath in training_data_files:
+	with wave.open(filepath, mode='rb') as fp:
+		raw_data = fp.readframes(fp.getnframes())
+		s = np.array(struct.unpack('<'+str(fp.getnframes())+'h', raw_data),
+					 dtype='int32')
+	with h5py.File(path+'training_data_{}.hdf5'.format(count), 'w') as fp:
+		fp.create_dataset("clean", data=s)
 
+		# Add 90 types of noise at SNR = 5dB and save each one
+		noise_count = 1
+		P_s = np.sum(np.abs(s)**2)/len(s)  # signal power
+		for noise in train_noise:
+			if len(noise) >= len(s):
+				noise_ext = noise[:len(s)]
+			else:
+				num_repeats = int(np.ceil(len(s)/len(noise)))
+				noise_ext = noise
+				for n in range(num_repeats):
+					noise_ext = np.concatenate((noise_ext, noise))
+				noise_ext = noise_ext[:len(s)]
 
-# Do the same for test data
+			# Adjust SNR
+			P_n = np.sum(np.abs(noise_ext)**2)/len(noise_ext)
+			k = np.sqrt(P_n/P_s)*10**(.25)  # amplitude adjustment factor
+			s_noisy = s + noise_ext/k
+			s_noisy = s_noisy.astype('int32')
+
+			fp.create_dataset("n{:03d}".format(noise_count), data=s_noisy)
+			noise_count += 1
+
+	count += 1
+
+# Testing data--one of the same utterances from female speaker from DR2,
+# one of the same utterances from female speaker from DR1 and DR3
+# All with testing noise types
+testing_data_files = [path+'originals/train/f2_SA1.WAV',
+					  path+'originals/train/f1_SA1.WAV',
+					  path+'originals/train/f3_SA1.WAV']
+
+count = 0
+for filepath in testing_data_files:
+	with wave.open(filepath, mode='rb') as fp:
+		raw_data = fp.readframes(fp.getnframes())
+		s = np.array(struct.unpack('<'+str(fp.getnframes())+'h', raw_data),
+					 dtype='int32')
+	with h5py.File(path+'testing_data_{}.hdf5'.format(count), 'w') as fp:
+		fp.create_dataset("clean", data=s)
+
+		# Add 10 types of non-stationary noise and save each one
+		noise_count = 91
+		P_s = np.sum(np.abs(s)**2)/len(s)  # signal power
+		for noise in test_noise:
+			if len(noise) >= len(s):
+				noise_ext = noise[:len(s)]
+			else:
+				num_repeats = int(np.ceil(len(s)/len(noise)))
+				noise_ext = noise
+				for n in range(num_repeats):
+					noise_ext = np.concatenate((noise_ext, noise))
+				noise_ext = noise_ext[:len(s)]
+
+			# Adjust SNR
+			P_n = np.sum(np.abs(noise_ext)**2)/len(noise_ext)
+			k = np.sqrt(P_n/P_s)*10**(.25)  # amplitude adjustment factor
+			s_noisy = s + noise_ext/k
+			s_noisy = s_noisy.astype('int32')
+
+			fp.create_dataset("n{:03d}".format(noise_count), data=s_noisy)
+			noise_count += 1
+
+		# Add stationary noise (WGN, 0 mean, unit variance)
+		white_noise = np.random.normal(0,1,len(s))
+		P_n = np.sum(np.abs(white_noise)**2)/len(white_noise)
+		k = np.sqrt(P_n/P_s)*10**(.25)
+		s_noisy = s + white_noise/k
+		s_noisy = s_noisy.astype('int32')
+
+		fp.create_dataset("WGN", data=s_noisy)
+
+	count += 1
