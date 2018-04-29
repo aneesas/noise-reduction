970a3de0b531610a45f9a03ad14d7ac582f2f352
Aneesa <aneesasona@gmail.com>
2018-04-29 10:58:59 -0400

    PESQ and STOI code checked and added

--- predict.py
+++ predict.py
@@ -10,6 +10,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 checkpoints_path = "/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/fcn_model"
 data_path = "/Users/aneesasonawalla/Documents/GT/ECE6255/Project/source/data/"
+save_path = data_path+"testwav/network1/"
 
 fcn_DAE = tf.estimator.Estimator(model_fn=fcn.fcn_model, model_dir=checkpoints_path)
 
@@ -31,5 +32,14 @@ predictions = list(fcn_DAE.predict(input_fn=predict_input_fn))
 
 predictions = [p["output"] for p in predictions]
 
+# Plot the stuff you care about, save it
+# Add title, axes labels, etc.
+plt.figure()
+plt.plot(eval_data_noisy[:50].reshape((50*512,)), linewidth=1.0, label="Noisy speech")
+plt.plot(np.hstack(predictions[:50]), linewidth=1.0, label="Predicted clean speech")
+plt.plot(eval_data_clean[:50].reshape((50*512,)), linewidth=1.0, label="Original clean speech")
+plt.legend()
+plt.savefig(save_path+"f2_SA1_n91.png")
+plt.show()
 
-# F2, 
+# Save data (50 predictions) as wav files in testwav to then calculate PESQ, STOI
